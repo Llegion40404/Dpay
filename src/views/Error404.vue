625b7d212a81a9dba292a869f10d60d94de52895
llegion40404 <pirojoktaken@gmail.com>
2023-06-16 22:20:43 -0700

    404 page fixed width

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="w-screen h-screen py-40 flex items-center justify-center text-center"
+    class="max-w-screen h-screen py-40 flex items-center justify-center text-center"
   >
-    <h1 class="text-emerald-600 phone:text-xl poco:text-2xl md:text-4xl">
+    <h1 class="text-emerald-600 phone:text-xl poco:text-2xl md:text-6xl">
       404 Page not found!
     </h1>
   </div>
